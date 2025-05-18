b237530d62c8247117ae05e215e36a131d594329
noodles <elvis.perlika@studio.unibo.it>
2025-05-18 21:16:58 +0200

    Initial Commit

---
+++ SyncToDo/SyncToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncToDoApp.swift
+//  SyncToDo
+//
+//  Created by Elvis Perlika on 18/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SyncToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
