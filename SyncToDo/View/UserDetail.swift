b7380ee6a9ed780323e3458b22ae01710bf72c99
noodles <elvis.perlika@studio.unibo.it>
2025-05-21 19:01:10 +0200

    feat: user list, user will become grey if touched

---
+++ SyncToDo/View/UserDetail.swift
@@ -0,0 +1,18 @@
+//
+//  UserDetail.swift
+//  SyncToDo
+//
+//  Created by Elvis Perlika on 21/05/25.
+//
+
+import SwiftUI
+
+struct UserDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDetail()
+}
